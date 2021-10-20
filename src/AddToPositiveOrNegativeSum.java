66f3d8567e8d187fdc40dfd7ba908bb876e4e30b
sseob2 <leesa@ssfs.org>
2021-10-20 09:47:12 -0400

    2 tasks

--- src/AddToPositiveOrNegativeSum.java
+++ src/AddToPositiveOrNegativeSum.java
@@ -2,16 +2,29 @@ import java.util.Scanner;
 public class AddToPositiveOrNegativeSum {
     public static void main(String[] args) {
         // declare and instantiate a Scanner variable and object
-
+        Scanner scan = new Scanner(System.in);
         // declare and initialize variables for a positive and negative sum
+        int positiveNum = 0;
+        int negativeNum = 0;
 
         for (int i = 1; i <= 10; i++) {
             /* in this block, allow the user to input a number and add the number to the appropriate sum */
+            System.out.print("Enter a number: ");
+            int UserInput = scan.nextInt();
 
+            if (UserInput > 0){
+                positiveNum += UserInput;
+            }
+
+            if (UserInput < 0){
+                negativeNum += UserInput;
+            }
 
         } // end of for loop
 
         // output the sums. Make sure the format matches the sample
+        System.out.println("The sum of the positive numbers is " + positiveNum);
+        System.out.println("The sum of the negative numbers is " + negativeNum);
 
     }
 }
--- src/FindLargest.java
+++ src/FindLargest.java
@@ -7,8 +7,24 @@ public class FindLargest {
         Scanner scan = new Scanner(System.in);
 
         // Prompt the user for two integer variables
+        System.out.print("Enter the first number: ");
+        int firstNumber = scan.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int secondNumber = scan.nextInt();
 
         // Create a series of if statements that will print which number is larger or if they are equal
+        if (firstNumber == secondNumber) {
+            System.out.println(firstNumber + " and " + secondNumber + " are equal");
+        }
+
+        if (firstNumber > secondNumber) {
+            System.out.println(firstNumber + " is larger than " +secondNumber);
+        }
+
+        if (firstNumber < secondNumber) {
+            System.out.println(secondNumber + " is larger than " + firstNumber);
+        }
         // Make sure your output matches the samples or the tests will not pass.
     }
 }
