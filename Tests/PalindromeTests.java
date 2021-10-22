199c02e62a50f8d015d86beb79f5d5e209da0762
sseob2 <leesa@ssfs.org>
2021-10-22 11:56:39 -0400

    1 task

--- Tests/PalindromeTests.java
+++ Tests/PalindromeTests.java
@@ -4,18 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PalindromeTests {
 
-//    @Test
-//    public void arePalindromes() {
-//        assertTrue(Palindrome.isPalindrome(12321));
-//        assertTrue(Palindrome.isPalindrome(94349));
-//        assertTrue(Palindrome.isPalindrome(88888));
-//    }
-//
-//    @Test
-//    public void areNotPalindromes() {
-//        assertFalse(Palindrome.isPalindrome(1961));
-//        assertFalse(Palindrome.isPalindrome(2442));
-//        assertFalse(Palindrome.isPalindrome(45674));
-//        assertFalse(Palindrome.isPalindrome(82682));
-//    }
+    @Test
+    public void arePalindromes() {
+        assertTrue(Palindrome.isPalindrome(12321));
+        assertTrue(Palindrome.isPalindrome(94349));
+        assertTrue(Palindrome.isPalindrome(88888));
+    }
+
+    @Test
+    public void areNotPalindromes() {
+        assertFalse(Palindrome.isPalindrome(1961));
+        assertFalse(Palindrome.isPalindrome(2442));
+        assertFalse(Palindrome.isPalindrome(45674));
+        assertFalse(Palindrome.isPalindrome(82682));
+    }
 }
--- src/Palindrome.java
+++ src/Palindrome.java
@@ -2,5 +2,27 @@ public class Palindrome {
 
     // Write your isPalindrome method in the space below. You do not need to write a main method for this program
     // Your method should be static.
+    public static boolean isPalindrome(int num){
 
+        int num2 = (int) (num * .1);
+        int num3 = num - num2 * 10;
+
+        int num4 = (num / 10000) * 10000;
+        int num5 = num - num4;
+        int num6 = (int) (num5 * 0.001);
+
+        int num7 = num / 100; // 123
+        int num8 = num - (num7 * 100); //21
+        int num9 = (int) (num8 * 0.1);
+
+
+        if ((num / 10000) == 0) {
+            return false;
+        } else if (num3 == num / 10000 && num6 == num9) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 }
