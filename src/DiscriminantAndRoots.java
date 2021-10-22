345e34c7f356dfc613b4fd77e684d06527637581
sseob2 <leesa@ssfs.org>
2021-10-22 11:31:56 -0400

    1 task

--- src/DiscriminantAndRoots.java
+++ src/DiscriminantAndRoots.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
+
 public class DiscriminantAndRoots {
 
     // Create the method roots in the space below.
     public static String roots(int a, int b, int c) {
 
-        return ""; // update this line so that it returns properly formatted output
+        double disVal = (Math.pow(b, 2)) - 4 * a * c;
+
+        if (disVal < 0) {
+            return a + "x^2 + " + b + "x + " + c + " has 2 imaginary roots";
+        } else if (disVal > 0) {
+            return a + "x^2 + " + b + "x + " + c + " has 2 real roots";
+        } else {
+            return a + "x^2 + " + b + "x + " + c + " has 1 real root";
+        }
+
     }
 
+    public static void main(String args[]){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the coefficients for a quadratic equation:");
+        int a1 = scan.nextInt();
+        int b1 = scan.nextInt();
+        int c1 = scan.nextInt();
+
+        System.out.println(roots(a1, b1, c1));
+
+    }
 }
